95c9a2fd77c5292fe81c083b42b201ae60d8d8f7
Rafaqfg <rafa.qfg@gmail.com>
2022-11-25 08:20:34 -0300

    feat: :sparkles: created menu_options function

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,19 @@
 # Requisito 12
+def menu_options():
+    print(
+        "Choose one option:"
+        "\n 0 - Fill the database with scrapped news;"
+        "\n 1 - Find news by title;"
+        "\n 2 - Find news by date;"
+        "\n 3 - Find news by tag;"
+        "\n 4 - Find news by category;"
+        "\n 5 - List top 5 news;"
+        "\n 6 - List top 5 categories;"
+        "\n 7 - exit."
+        "\n ",
+        end="",
+    )
+
+
 def analyzer_menu():
     """Seu código deve vir aqui"""
